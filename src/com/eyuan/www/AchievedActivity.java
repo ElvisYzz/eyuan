fec76e70e84b7d4e9ebd2505dcdc6ade4665d57e
zjh <zjhsdtc@gmail.com>
2013-07-24 17:56:52 +0800

    cleantha_v_1

---
+++ src/com/eyuan/www/AchievedActivity.java
@@ -0,0 +1,13 @@
+package com.eyuan.www;
+
+import android.os.Bundle;
+import net.tsz.afinal.FinalActivity;
+
+public class AchievedActivity extends FinalActivity{
+	 @Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.achievedwish);
+	}
+}
---
+++ src/com/eyuan/www/SubmitActivity.java
@@ -0,0 +1,14 @@
+package com.eyuan.www;
+
+import android.os.Bundle;
+import net.tsz.afinal.FinalActivity;
+
+public class SubmitActivity extends FinalActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.submit);
+	}
+}
