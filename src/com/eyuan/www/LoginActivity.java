f303cd9eeddffab249381690f62ede4906e3d1a4
zjh <zjhsdtc@gmail.com>
2013-07-25 21:14:36 +0800

    rebuilt register login splash activities and modified something in /res/values

---
+++ src/com/eyuan/www/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.eyuan.www;
+
+import android.os.Bundle;
+import net.tsz.afinal.FinalActivity;
+
+public class LoginActivity extends FinalActivity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.login);
+	}
+
+}
